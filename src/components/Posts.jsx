62d0d697f8574b60e5f60d006a848da67817e502
nadajaradat <nadajaradat2020@gmail.com>
2023-10-14 23:49:02 +0300

    home

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { formatDistanceToNow } from 'date-fns'; // Import the formatDistanceToNow function
 import { useDrawer } from '../contexts/OpenDrawer';
-import test from '../assets/ExplorePics/explorePic13.webp';
 import { Avatar, Box, List, Typography } from '@mui/material';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
@@ -8,9 +8,7 @@ import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 
 function Posts({ posts }) {
-  // Sort the posts array in reverse order based on createdAt
   const sortedPosts = [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
   const { open, toggleDrawer } = useDrawer();
 
   return (
@@ -30,7 +28,7 @@ function Posts({ posts }) {
               style={{ backgroundColor: 'black', width: '38px', height: '38px' }}
             />
             <Typography variant="caption" color={'white'} style={{ marginTop: '10px', marginLeft: '10px' }}>
-              Nada Jaradat .8h
+              Nada Jaradat - {formatDistanceToNow(new Date(post.createdAt))} ago
             </Typography>
           </div>
 
